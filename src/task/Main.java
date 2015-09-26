ea3776ca1bda0f0f195ea48723f0a4f86a39b9cf
zlatkov <alexander.z.zlatkov@gmail.com>
2015-09-26 18:06:39 +0300

    Reading input

--- src/task/Main.java
+++ src/task/Main.java
@@ -1,21 +1,38 @@
 package task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Main {
 
+    private static final String TERMINATION_WORD = "exit";
+
     public static void main(String[] args) {
+        try {
+            String text = readInput(System.in);
 
-        String text = "A \"concordance\" is an alphabetical list of the words present in a text with a count of how\n" +
-                "often each word appears and citations of where each word appears in the text (e.g., page\n" +
-                "number). Write a program -- in the programming language of your choice -- that will\n" +
-                "generate a concordance of an arbitrary text document written in English: the text can be\n" +
-                "read from stdin, and the program should output the concordance to stdout or a file. For\n" +
-                "each word, it should print the count and the sorted list of citations, in this case the\n" +
-                "zero-indexed sentence number in which that word occurs. You may assume that the input\n" +
-                "contains only spaces, newlines, standard English letters, and standard English punctuation\n" +
-                "marks.";
+            ConcordanceCalculator calculator = new ConcordanceCalculator(text);
+            Concordance concordance = calculator.calculate();
+            System.out.println(concordance);
+        }
+        catch (IOException e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
 
-        ConcordanceCalculator calculator = new ConcordanceCalculator(text);
-        Concordance concordance = calculator.calculate();
-        System.out.println(concordance);
+    private static String readInput(InputStream stream) throws IOException{
+        InputStreamReader streamReader = new InputStreamReader(stream);
+        BufferedReader reader = new BufferedReader(streamReader);
+        StringBuilder result = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null && !line.equals(TERMINATION_WORD)) {
+            result.append(line);
+            result.append("\n");
+        }
+
+        return result.toString();
     }
 }
